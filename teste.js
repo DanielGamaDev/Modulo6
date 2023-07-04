e361126aa7d0dde478e27a42e0055a114272fd0d
Daniel Gama <danielgama.dev@gmail.com>
2023-07-04 20:28:54 -0300

    Initial commit

---
+++ teste.js
@@ -0,0 +1,12 @@
+let altura = prompt("Qual a altura?");
+
+let largura = prompt("Qual a largura?");
+
+function calculaAreaTotal(altura, largura){
+    a = altura;
+    l = largura;
+    areatotal = (a*l);
+    return areatotal;
+} 
+
+alert ("A area total é igual a " + areatotal)
